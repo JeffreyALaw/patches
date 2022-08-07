b1fdf81ab226f618dc020e57764a18bd238267f3
Jeff Law <jeffreyalaw@gmail.com>
2022-08-07 09:48:49 -0600

    Try again to disable libsaniitzer

--- jobs/native.sh
+++ jobs/native.sh
@@ -23,7 +23,7 @@ make install-gas install-binutils install-ld
 popd
 
 pushd obj/gcc
-../../gcc/configure --prefix=$PREFIX --disable-analyzer --prefix=$PREFIX --enable-languages=c,c++,fortran --disable-multilib --disable-sanitizer
+../../gcc/configure --prefix=$PREFIX --disable-analyzer --prefix=$PREFIX --enable-languages=c,c++,fortran --disable-multilib --disable-libsanitizer
 make -j $NPROC -l $NPROC
 make install
 popd
