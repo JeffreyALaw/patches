9002ade78832a056c2d065c0b1a48d53db62357a
Jeff Law <jeffreyalaw@gmail.com>
2023-08-24 08:06:15 -0600

    Disable bootstrap for now while I debug things for qemu emulated

--- jobs/native-emulated.sh
+++ jobs/native-emulated.sh
@@ -29,7 +29,7 @@ make install-gas install-binutils install-ld
 popd
 
 pushd obj/gcc
-../../gcc/configure --prefix=$PREFIX --disable-analyzer --prefix=$PREFIX --enable-languages=c,c++,fortran,lto --disable-multilib --disable-libsanitizer
+../../gcc/configure --prefix=$PREFIX --disable-analyzer --prefix=$PREFIX --enable-languages=c,c++,fortran,lto --disable-multilib --disable-libsanitizer --disable-bootstrap
 make -j $NPROC -l $NPROC
 make install
 popd
