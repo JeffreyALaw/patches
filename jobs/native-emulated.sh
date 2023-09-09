e83433223e34ffc750c4b2794ede5aa46a701176
Jeff Law <jeffreyalaw@gmail.com>
2023-09-09 14:28:17 -0600

    alpha can't build BFD righ tnow

--- jobs/native-emulated.sh
+++ jobs/native-emulated.sh
@@ -22,11 +22,13 @@ mkdir -p obj/{binutils-gdb,gcc,glibc,linux}
 export PREFIX=`pwd`/installed
 PATH=`pwd`/installed/bin:/home/law/bin:$PATH
 
-pushd obj/binutils-gdb
-../../binutils-gdb/configure --enable-warn-rwx-segments=no --enable-warn-execstack=no --prefix=$PREFIX ${TARGET}
-make -j $NPROC -l $NPROC all-gas all-binutils all-ld 
-make install-gas install-binutils install-ld
-popd
+if [ $target != alpha-linux-gnu ]; then
+  pushd obj/binutils-gdb
+  ../../binutils-gdb/configure --enable-warn-rwx-segments=no --enable-warn-execstack=no --disable-werror --prefix=$PREFIX ${TARGET}
+  make -j $NPROC -l $NPROC all-gas all-binutils all-ld 
+  make install-gas install-binutils install-ld
+  popd
+fi
 
 pushd obj/gcc
 ../../gcc/configure --prefix=$PREFIX --disable-analyzer --prefix=$PREFIX --enable-languages=c,c++,fortran,lto --disable-multilib --disable-libsanitizer  ${TARGET}
