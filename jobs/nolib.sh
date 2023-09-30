52c77e1a8a50e3e7806beb77cb9e26e172723ba3
Jeff Law <jlaw@ventanamicro.com>
2023-09-29 21:36:19 -0600

    First crack at not emitting the full log file, but instead keeping it as an artifact

--- jobs/nolib.sh
+++ jobs/nolib.sh
@@ -4,10 +4,8 @@
 set -e
 set -o pipefail
 
-# To facilitate debugging if there is a host side issue
-#hostname
-
 TARGET=$1
+LOGFILE=`pwd`/${TARGET}/log.gz
 
 NPROC=`nproc --all`
 
@@ -22,9 +20,9 @@ patches/jobs/setupsources.sh $TARGET master binutils-gdb gcc
 
 # Step 1, build binutils
 cd ${TARGET}-obj/binutils
-../../binutils-gdb/configure --prefix=`pwd`/../../${TARGET}-installed --target=${TARGET}
-make -j $NPROC -l $NPROC all-gas all-binutils all-ld
-make install-gas install-binutils install-ld
+../../binutils-gdb/configure --prefix=`pwd`/../../${TARGET}-installed --target=${TARGET} >& $LOGFILE
+make -j $NPROC -l $NPROC all-gas all-binutils all-ld >& $LOGFILE
+make install-gas install-binutils install-ld >& $LOGFILE
 cd ../..
 cd ${TARGET}-installed/bin
 rm -f ar as ld ld.bfd nm objcopy objdump ranlib readelf strip
@@ -33,9 +31,9 @@ cd ../..
 # Step 2, build gcc
 PATH=`pwd`/${TARGET}-installed/bin:$PATH
 cd ${TARGET}-obj/gcc
-../../gcc/configure --disable-analyzer --with-system-libunwind --with-newlib --without-headers --disable-threads --disable-shared --enable-languages=c,c++,lto --prefix=`pwd`/../../${TARGET}-installed --target=${TARGET}
-make -j $NPROC -l $NPROC all-gcc
-make install-gcc
+../../gcc/configure --disable-analyzer --with-system-libunwind --with-newlib --without-headers --disable-threads --disable-shared --enable-languages=c,c++,lto --prefix=`pwd`/../../${TARGET}-installed --target=${TARGET} >& $LOGFILE
+make -j $NPROC -l $NPROC all-gcc >& $LOGFILE
+make install-gcc >& $LOGFILE
 
 # We try to build and install libgcc, but don't consider a failure fatal
 (make -j $NPROC -l $NPROC all-target-libgcc && make install-target-libgcc) || /bin/true
