3c0c8e1753ea08928c36f397f597790913925c2d
Jeff Law <jeffreyalaw@gmail.com>
2023-11-26 22:16:16 -0700

    Do redirections for linux-update tests too

--- jobs/linux-update.sh
+++ jobs/linux-update.sh
@@ -7,6 +7,7 @@ set -o pipefail
 #hostname
 
 TARGET=$1
+LOGFILE=`pwd`/log
 
 
 NPROC=`nproc --all`
@@ -24,13 +25,14 @@ mkdir -p ${TARGET}-obj/glibc
 
 export RUNGCCTESTS=yes
 
-patches/jobs/setupsources.sh ${TARGET} master binutils-gdb gcc glibc linux
+patches/jobs/setupsources.sh ${TARGET} master binutils-gdb gcc glibc linux >& $LOGFILE 2>&1
 
 # Step 1, build binutils
+echo Building binutils
 cd ${TARGET}-obj/binutils
-../../binutils-gdb/configure --enable-warn-rwx-segments=no --enable-warn-execstack=no --prefix=`pwd`/../../${TARGET}-installed --target=${TARGET}
-make -j $NPROC -l $NPROC all-gas all-binutils all-ld
-make install-gas install-binutils install-ld
+../../binutils-gdb/configure --enable-warn-rwx-segments=no --enable-warn-execstack=no --prefix=`pwd`/../../${TARGET}-installed --target=${TARGET} >> $LOGFILE 2>&1
+make -j $NPROC -l $NPROC all-gas all-binutils all-ld >> $LOGFILE 2>&1
+make install-gas install-binutils install-ld >> $LOGFILE 2>&1
 cd ../..
 cd ${TARGET}-installed/bin
 rm -f ar as ld ld.bfd nm objcopy objdump ranlib readelf strip
@@ -42,22 +44,25 @@ if [ \! `find ${TARGET}-installed -name crti.o` ]; then
 fi
 
 # Step 2, build gcc
+echo Building GCC
 PATH=`pwd`/${TARGET}-installed/bin:$PATH
 cd ${TARGET}-obj/gcc
-../../gcc/configure $SHARED --disable-analyzer --disable-multilib --without-headers --disable-threads --enable-languages=c,c++,fortran,lto --prefix=`pwd`/../../${TARGET}-installed --target=${TARGET}
-make -j $NPROC -l $NPROC all-gcc
-make install-gcc
+../../gcc/configure $SHARED --disable-analyzer --disable-multilib --without-headers --disable-threads --enable-languages=c,c++,fortran,lto --prefix=`pwd`/../../${TARGET}-installed --target=${TARGET} >> $LOGFILE 2>&1
+make -j $NPROC -l $NPROC all-gcc >> $LOGFILE 2>&1
+make install-gcc >> $LOGFILE 2>&1
 
 # Step 2.1, build and install libgcc.  This is separate from the other libraries because
 # it should always work.  Other libraries may not work the first time we build on a host
 # because the target headers, crt files, etc from glibc are not yet available.
-make -j $NPROC -l $NPROC all-target-libgcc
-make install-target-libgcc
+echo Building libgcc
+make -j $NPROC -l $NPROC all-target-libgcc >> $LOGFILE 2>&1
+make install-target-libgcc >> $LOGFILE 2>&1
 
 # Step 2.2.  If it looks like we have crt files, then build libstdc++-v3
 if [ -f ../../${TARGET}-installed/${TARGET}/lib/crt1.o ]; then
-  make -j $NPROC -l $NPROC all-target-libstdc++-v3
-  make install-target-libstdc++-v3
+  echo Building libstdc++
+  make -j $NPROC -l $NPROC all-target-libstdc++-v3 >> $LOGFILE 2>&1
+  make install-target-libstdc++-v3 >> $LOGFILE 2>&1
 fi
 
 cd ../..
@@ -138,15 +143,16 @@ case "${TARGET}" in
 esac
 
 cd ${TARGET}-obj/linux
-make -C ../../linux O=${TARGET} ARCH=${ARCH} INSTALL_HDR_PATH=../../${TARGET}-installed/${TARGET} headers_install
+echo Building kernel
+make -C ../../linux O=${TARGET} ARCH=${ARCH} INSTALL_HDR_PATH=../../${TARGET}-installed/${TARGET} headers_install >> $LOGFILE 2>&1
 
 if [ $KERNEL == "true" ]; then
-  make -C ../../linux O=${TARGET} ARCH=${ARCH} mrproper
-  make -C ../../linux O=${TARGET} ARCH=${ARCH} CROSS_COMPILE=${TARGET}- -j $NPROC -l $NPROC $CONFIG
+  make -C ../../linux O=${TARGET} ARCH=${ARCH} mrproper >> $LOGFILE 2>&1
+  make -C ../../linux O=${TARGET} ARCH=${ARCH} CROSS_COMPILE=${TARGET}- -j $NPROC -l $NPROC $CONFIG >> $LOGFILE 2>&1
   if [ $STRIPKGDBCONFIG == "yes" ]; then
     sed -i -e s/CONFIG_KGDB=y/CONFIG_KGDB=n/ ../../linux/${TARGET}/.config
   fi
-  make -C ../../linux O=${TARGET} ARCH=${ARCH} CROSS_COMPILE=${TARGET}- -j $NPROC -l $NPROC $KERNEL_TARGETS
+  make -C ../../linux O=${TARGET} ARCH=${ARCH} CROSS_COMPILE=${TARGET}- -j $NPROC -l $NPROC $KERNEL_TARGETS >> $LOGFILE 2>&1
 fi
 
 cd ../..
@@ -154,10 +160,11 @@ cd ../..
 
 
 # Step 4, build glibc
+echo Building glibc
 cd ${TARGET}-obj/glibc
-../../glibc/configure --prefix=`pwd`/../../${TARGET}-installed/${TARGET} --build=x86_64-linux-gnu --host=${TARGET} --enable-add-ons --disable-werror
-make -j $NPROC -l $NPROC 
-make install
+../../glibc/configure --prefix=`pwd`/../../${TARGET}-installed/${TARGET} --build=x86_64-linux-gnu --host=${TARGET} --enable-add-ons --disable-werror >> $LOGFILE 2>&1
+make -j $NPROC -l $NPROC  >> $LOGFILE 2>&1
+make install >> $LOGFILE 2>&1
 cd ../..
 
 # Step 5, run tests
