48fd74d6818701dc1960338f42454ad29e775a85
Jeff Law <jeffreyalaw@gmail.com>
2025-01-14 19:54:02 -0700

    Revert "Do the GCC build with output to the console to help debug sh4 issue"
    This reverts commit 57ea1c5447c30ffc13edb6c14c7ca5e0cfc5778e.

--- jobs/native-emulated.sh
+++ jobs/native-emulated.sh
@@ -49,10 +49,8 @@ echo Building GCC
 date
 pushd obj/gcc
 ../../gcc/configure --prefix=$PREFIX --disable-analyzer --prefix=$PREFIX --enable-languages=c,c++,fortran,lto $ENABLE_MULTIARCH --disable-multilib --disable-libsanitizer $DISABLE_BOOTSTRAP ${TARGET} >> $LOGFILE 2>&1
-#make -j $NPROC -l $NPROC >> $LOGFILE 2>&1
-#make install >> $LOGFILE 2>&1
-make -j $NPROC -l $NPROC >> $LOGFILE
-make install >> $LOGFILE
+make -j $NPROC -l $NPROC >> $LOGFILE 2>&1
+make install >> $LOGFILE 2>&1
 popd
 
 case ${TARGET} in
