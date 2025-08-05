bbf75172952efd95004e2f6b654badcd41260ff5
Jeff Law <jeffreyalaw@gmail.com>
2025-08-05 17:14:18 -0600

    Set $TARGET earlier

--- jobs/newlib.sh
+++ jobs/newlib.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Ensure that we're in a sensible directory
+TARGET=$1
 cd /home/jlaw/jenkins/workspace/$TARGET
 
 # Ensure that we fail if any command fails
 set -e
 set -o pipefail
 
-TARGET=$1
 LOGFILE=`pwd`/log
 
 NPROC=`nproc --all`
